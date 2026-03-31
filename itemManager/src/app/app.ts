4268f24c315e521d647714ccc1449e94aca396f9
neylorxt <neylorxt@gmail.com>
2026-03-31 14:44:16 +0200

    Clôture du projet ItemManager

--- itemManager/src/app/app.ts
+++ itemManager/src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { Item } from './components/item/item';
 import { ItemModel } from './components/model/item.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Item],
+  imports: [RouterOutlet, Item, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -13,8 +14,36 @@ export class App {
   protected readonly title = signal('itemManager');
 
   items: ItemModel[] = [
-    new ItemModel('Item 1', 'Type A', 'This is a description of Item 1'),
-    new ItemModel('Item 2', 'Type B', 'This is a description of Item 2'),
-    new ItemModel('Item 3', 'Type C', 'This is a description of Item 3'),
+    new ItemModel(
+      'Coin',
+      'Currency',
+      'This is a description of a coin item',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Coin_video_game.png/250px-Coin_video_game.png',
+    ),
+    new ItemModel(
+      'Gimp',
+      'Tool',
+      'This is a description of a gimp item',
+      'https://www.wikidebrouillard.org/images/thumb/9/99/Item-The_Gimp_gimp.png/768px-Item-The_Gimp_gimp.png',
+    ),
+    new ItemModel(
+      'Adobe',
+      'Software',
+      'This is a description of a adobe item',
+      'https://static.vecteezy.com/system/resources/previews/060/100/954/non_2x/adobe-logo-free-download-adobe-logo-free-png.png',
+    ),
   ];
+
+  researchModel = signal('');
+
+  onSearchInput(value: string) {
+    this.researchModel.set(value);
+  }
+
+  // Computed property to filter items based on the search input
+  filteredItems = computed(() => {
+    const search = this.researchModel().toLowerCase();
+
+    return this.items.filter((item) => item.name.toLowerCase().includes(search));
+  });
 }
--- itemManager/src/app/components/model/item.model.ts
+++ itemManager/src/app/components/model/item.model.ts
@@ -2,13 +2,15 @@ export class ItemModel {
   name: string;
   type: string;
   description: string;
+  src: string = 'https://via.placeholder.com/150';
 
   constructor();
-  constructor(name: string, type: string, description: string);
+  constructor(name: string, type: string, description: string, src: string);
 
-  constructor(name?: string, type?: string, description?: string) {
+  constructor(name?: string, type?: string, description?: string, src?: string) {
     this.name = name || '';
     this.type = type || '';
     this.description = description || '';
+    this.src = src || this.src;
   }
 }
